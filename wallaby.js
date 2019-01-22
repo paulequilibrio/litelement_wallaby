d75601c89c6e5a16cb0edb7969c36fc09abe978f
Paulo Alexandre <paulequilibrio@gmail.com>
2019-01-21 23:00:59 -0300

    Use open-wc in Wallaby.js configuration
    The package wallaby-webpack is a dependency of @open-wc/testing-wallaby
    for the bundling process needed for the tests with Wallaby.

--- wallaby.js
+++ wallaby.js
@@ -1,36 +1,17 @@
-module.exports = function (wallaby) {
-  return {
-    files: [
-      {
-        pattern: 'node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js',
-        instrument: false
-      },
-      {
-        pattern: 'node_modules/chai/chai.js',
-        instrument: false
-      },
-      { pattern: 'src/*.js', load: false }
-    ],
+const wallabyWebpack = require('wallaby-webpack')
+const wallabyPostprocessor = wallabyWebpack()
 
-    tests: ['test/*.test.js'],
-
-    env: {
-      kind: 'chrome',
-      runner: '/usr/bin/google-chrome-stable'
-    },
-
-    testFramework: 'mocha',
-    debug: true,
-
-    // delaying running tests until web components are ready
-    setup: function (wallaby) {
-      if (!window.CustomElements || !window.CustomElements.readyTime) {
-        wallaby.delayStart()
-        window.addEventListener('WebComponentsReady', function () {
-          wallaby.start()
-        })
-      }
-      window.assert = chai.assert
-    }
+module.exports = () => ({
+  files: [ { pattern: 'src/*.js', load: false } ],
+  tests: [ { pattern: 'test/*.test.js', load: false } ],
+  testFramework: 'mocha',
+  debug: true,
+  env: {
+    kind: 'chrome'
+  },
+  postprocessor: wallabyPostprocessor,
+  setup: () => {
+    // required to trigger test loading
+    window.__moduleBundler.loadTests()
   }
-}
+})
