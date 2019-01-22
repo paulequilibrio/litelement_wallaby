893b2f7bb652e2a71ab3a3cdcaf1d15b326cb320
Paulo Alexandre <paulequilibrio@gmail.com>
2019-01-21 22:41:15 -0300

    Use open-wc in Karma configuration
    Open-wc packages help with the bundling process needed for the tests.

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,15 @@
-module.exports = function (config) {
-  config.set({
-    frameworks: ['mocha', 'chai'],
-    // list of files / patterns to load in the browser
-    files: [
-      'src/**/*.js',
-      'test/**/*.test.js'
-    ],
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
-    autoWatch: false,
-    // singleRun: false, // Karma captures browsers, runs the tests and exits
-    concurrency: Infinity
-  })
+const defaultSettings = require('@open-wc/testing-karma/default-settings.js')
+const merge = require('webpack-merge')
+
+module.exports = config => {
+  config.set(
+    merge(defaultSettings(config), {
+      files: [
+        // allows running single tests with the --grep flag
+        config.grep ? config.grep : 'test/**/*.test.js'
+      ]
+      // your custom config
+    })
+  )
+  return config
 }
